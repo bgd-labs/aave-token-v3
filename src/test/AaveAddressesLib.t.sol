bb1aa8fa17ae7527599bcf5023ae012a78d0005c
Andrey Kozlov <kyzia.ru@gmail.com>
2022-05-17 21:54:24 +0200

    forge install: aave-token-v2

---
+++ src/test/AaveAddressesLib.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library AaveAddressesLib {
+    address[] public constant AAVE_HOLDERS = [
+        0x4da27a545c0c5b758a6ba100e3a049001de870f5,
+        0xffc97d72e13e01096502cb8eb52dee56f74dad7b,
+        0x25f2226b597e8f9514b3f68f00f494cf4f286491,
+        0xc697051d1c6296c24ae3bcef39aca743861d9a81,
+        0xbe0eb53f46cd790cd13851d5eff43d12404d33e8,
+        0x317625234562b1526ea2fac4030ea499c5291de4,
+        0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503,
+        0xf977814e90da44bfa03b6295a0616a897441acec,
+        0x26a78d5b6d7a7aceedd1e6ee3229b372a624d8b7,
+        0x28c6c06298d514db089934071355e5743bf21d60
+    ];
+}
