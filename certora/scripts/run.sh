5bffdd862f4fafe0090f4dca1c291591afcfeb4a
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2022-05-25 11:45:53 +0300

    removing unnecessary files

--- certora/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-certoraRun src/AaveTokenV3.sol:AaveTokenV3 \
-    --verify AaveTokenV3:certora/specs/complexity.spec \
-    --solc solc8.13 \
-    --optimistic_loop \
-    --staging \
-    --msg "AaveTokenV3 complexity check"
- 
