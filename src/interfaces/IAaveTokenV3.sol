0c8f7a4beadfed7dba3351594cab5c71de7efdf3
Harsh Pandey <harshsatishpandey@gmail.com>
2023-03-22 13:34:27 +0530

    fix: interface import

--- src/interfaces/IAaveTokenV3.sol
+++ src/interfaces/IAaveTokenV3.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.0;
 import {IERC20Metadata} from 'openzeppelin-contracts/token/ERC20/extensions/IERC20Metadata.sol';
 import {IERC20Permit} from 'openzeppelin-contracts/token/ERC20/extensions/draft-IERC20Permit.sol';
 import {IGovernancePowerDelegationToken} from '../interfaces/IGovernancePowerDelegationToken.sol';
-import {IERC20} from 'openzeppelin-contracts/token/ERC20/IERC20.sol';
 
-interface IAaveTokenV3 is IERC20, IERC20Metadata, IERC20Permit, IGovernancePowerDelegationToken {
+interface IAaveTokenV3 is IERC20Metadata, IERC20Permit, IGovernancePowerDelegationToken {
   /**
    * @notice Returns the current nonce for `owner`.
    * @dev Backward compatible version of nonces
