9837be50ad94537ca61712dfb26a3bdbef0a49c5
Andrei Kozlov <kyzia.ru@gmail.com>
2023-03-22 10:54:48 +0100

    remove not used interface method, change some docs to inheridoc

--- src/BaseAaveTokenV2.sol
+++ src/BaseAaveTokenV2.sol
@@ -14,6 +14,7 @@ abstract contract BaseAaveTokenV2 is BaseAaveToken, IERC20Permit, VersionedIniti
   uint256[3] private ______DEPRECATED_FROM_AAVE_V1;
   ///////// @dev END OF DEPRECATED from AaveToken v1  //////////////////////////
 
+  /// @inheritdoc IERC20Permit
   bytes32 public DOMAIN_SEPARATOR;
 
   ///////// @dev DEPRECATED from AaveToken v2  //////////////////////////
@@ -29,17 +30,7 @@ abstract contract BaseAaveTokenV2 is BaseAaveToken, IERC20Permit, VersionedIniti
 
   uint256 public constant REVISION = 3;
 
-  /**
-   * @dev implements the permit function as for https://github.com/ethereum/EIPs/blob/8a34d644aacf0f9f8f00815307fd7dd5da07655f/EIPS/eip-2612.md
-   * @param owner the owner of the funds
-   * @param spender the spender
-   * @param value the amount
-   * @param deadline the deadline timestamp, type(uint256).max for no deadline
-   * @param v signature param
-   * @param s signature param
-   * @param r signature param
-   */
-
+  /// @inheritdoc IERC20Permit
   function permit(
     address owner,
     address spender,
@@ -69,9 +60,7 @@ abstract contract BaseAaveTokenV2 is BaseAaveToken, IERC20Permit, VersionedIniti
     _approve(owner, spender, value);
   }
 
-  /**
-   * @dev Updated version of _nonces
-   */
+  /// @inheritdoc IERC20Permit
   function nonces(address owner) external view returns (uint256) {
     return _nonces[owner];
   }
--- src/interfaces/IAaveTokenV3.sol
+++ src/interfaces/IAaveTokenV3.sol
@@ -6,12 +6,6 @@ import {IERC20Permit} from 'openzeppelin-contracts/token/ERC20/extensions/draft-
 import {IGovernancePowerDelegationToken} from '../interfaces/IGovernancePowerDelegationToken.sol';
 
 interface IAaveTokenV3 is IERC20Metadata, IERC20Permit, IGovernancePowerDelegationToken {
-  /**
-   * @notice Returns the current nonce for `owner`.
-   * @dev Backward compatible version of nonces
-   */
-  function _nonces(address owner) external view returns (uint256);
-
   function DELEGATE_BY_TYPE_TYPEHASH() external view returns (bytes32);
 
   function PERMIT_TYPEHASH() external view returns (bytes32);
