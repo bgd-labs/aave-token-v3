a7ea7456dbef4cca3069abbf9a262bc10cfca682
Andrei Kozlov <kyzia.ru@gmail.com>
2023-05-08 16:09:50 +0900

    EIP712 contract integration

--- src/BaseAaveTokenV2.sol
+++ src/BaseAaveTokenV2.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import {VersionedInitializable} from './utils/VersionedInitializable.sol';
 import {BaseAaveToken} from './BaseAaveToken.sol';
+import {EIP712} from '../lib/openzeppelin/contracts/utils/cryptography/EIP712.sol';
+import {ECDSA} from '../lib/openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 
-abstract contract BaseAaveTokenV2 is BaseAaveToken, VersionedInitializable {
+abstract contract BaseAaveTokenV2 is BaseAaveToken, VersionedInitializable, EIP712 {
   /// @dev owner => next valid nonce to submit with permit()
   mapping(address => uint256) public _nonces;
 
@@ -13,20 +15,24 @@ abstract contract BaseAaveTokenV2 is BaseAaveToken, VersionedInitializable {
   uint256[3] private ______DEPRECATED_FROM_AAVE_V1;
   ///////// @dev END OF DEPRECATED from AaveToken v1  //////////////////////////
 
-  bytes32 public DOMAIN_SEPARATOR;
+  // deprecated in favor to OZ EIP712
+  bytes32 private __DEPRECATED_DOMAIN_SEPARATOR;
 
   ///////// @dev DEPRECATED from AaveToken v2  //////////////////////////
   //////// kept for backwards compatibility with old storage layout ////
   uint256[4] private ______DEPRECATED_FROM_AAVE_V2;
   ///////// @dev END OF DEPRECATED from AaveToken v2  //////////////////////////
 
-  bytes public constant EIP712_REVISION = bytes('1');
-  bytes32 internal constant EIP712_DOMAIN =
-    keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)');
   bytes32 public constant PERMIT_TYPEHASH =
     keccak256('Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)');
 
   uint256 public constant REVISION = 3;
+
+  constructor() EIP712('Aave token V3', 'V2') {}
+
+  function DOMAIN_SEPARATOR() public view returns (bytes32) {
+    return _domainSeparatorV4();
+  }
 
   /**
    * @dev implements the permit function as for https://github.com/ethereum/EIPs/blob/8a34d644aacf0f9f8f00815307fd7dd5da07655f/EIPS/eip-2612.md
@@ -52,15 +58,11 @@ abstract contract BaseAaveTokenV2 is BaseAaveToken, VersionedInitializable {
     //solium-disable-next-line
     require(block.timestamp <= deadline, 'INVALID_EXPIRATION');
     uint256 currentValidNonce = _nonces[owner];
-    bytes32 digest = keccak256(
-      abi.encodePacked(
-        '\x19\x01',
-        DOMAIN_SEPARATOR,
-        keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentValidNonce, deadline))
-      )
+    bytes32 digest = _hashTypedDataV4(
+      keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentValidNonce, deadline))
     );
 
-    require(owner == ecrecover(digest, v, r, s), 'INVALID_SIGNATURE');
+    require(owner == ECDSA.recover(digest, v, r, s), 'INVALID_SIGNATURE');
     unchecked {
       // does not make sense to check because it's not realistic to reach uint256.max in nonce
       _nonces[owner] = currentValidNonce + 1;
