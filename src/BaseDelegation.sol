872716d216aa5ff9934abdd0c315a358a4979c00
sendra <polsendra@gmail.com>
2023-06-22 15:58:05 +0200

    fix: use remmaping

--- src/BaseDelegation.sol
+++ src/BaseDelegation.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ECDSA} from '../lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol';
+import {ECDSA} from 'openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol';
 
 import {SafeCast72} from './utils/SafeCast72.sol';
 import {IGovernancePowerDelegationToken} from './interfaces/IGovernancePowerDelegationToken.sol';
