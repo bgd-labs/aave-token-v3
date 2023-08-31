a9d6cf6cadcfb881e6ebf03a1689c6ec5a1dbca1
Rustem Kurmaev <rustboyar@gmail.com>
2023-08-31 10:47:30 +0200

    e2eAsset token

--- src/test/payload/UpdateAaveTokenPayload.t.sol
+++ src/test/payload/UpdateAaveTokenPayload.t.sol
@@ -6,10 +6,12 @@ import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {ProxyHelpers} from 'aave-helpers/ProxyHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+
 import {AaveTokenV3} from '../../AaveTokenV3.sol';
 import {UpdateAaveTokenPayload} from '../../payload/UpdateAaveTokenPayload.sol';
 
-contract UpdateAaveTokenPayloadTest is Test {
+contract UpdateAaveTokenPayloadTest is ProtocolV3TestBase {
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 17635720);
   }
@@ -27,5 +29,13 @@ contract UpdateAaveTokenPayloadTest is Test {
     );
 
     assertEq(newImpl, address(aaveToken));
+
+    ReserveConfig[] memory allConfigs = _getReservesConfigs(AaveV3Ethereum.POOL);
+
+    e2eTestAsset(
+      AaveV3Ethereum.POOL,
+      _findReserveConfig(allConfigs, AaveV3EthereumAssets.USDC_UNDERLYING),
+      _findReserveConfig(allConfigs, AaveV3EthereumAssets.AAVE_UNDERLYING)
+    );
   }
 }
