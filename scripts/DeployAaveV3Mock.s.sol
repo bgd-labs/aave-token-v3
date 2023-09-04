4b821b7494bac01051f86ec7e488ae6776ded394
sendra <polsendra@gmail.com>
2023-09-04 13:15:33 +0200

    fix: updated with verifying token

--- scripts/DeployAaveV3Mock.s.sol
+++ scripts/DeployAaveV3Mock.s.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import 'forge-std/console.sol';
+//import 'forge-std/console.sol';
 import 'forge-std/Script.sol';
 import {TransparentUpgradeableProxy} from 'solidity-utils/contracts/transparent-proxy/TransparentUpgradeableProxy.sol';
 import {AaveMisc} from 'aave-address-book/AaveMisc.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
 import {AaveTokenV3} from '../src/AaveTokenV3.sol';
-import {EthereumScript} from 'aave-helpers/ScriptUtils.sol';
-import {TransparentProxyFactory} from "solidity-utils/contracts/transparent-proxy/TransparentProxyFactory.sol";
+import {ITransparentProxyFactory} from "solidity-utils/contracts/transparent-proxy/TransparentProxyFactory.sol";
 
 contract AaveTokenV3Mock is AaveTokenV3 {
   function name() public view virtual override returns (string memory) {
@@ -36,7 +35,7 @@ contract DeployAaveV3Mock is Script {
     AaveTokenV3 aaveTokenImpl = new AaveTokenV3Mock();
     aaveTokenImpl.initialize();
 
-    TransparentProxyFactory proxyFactory = 0x98C977c66266366dbEc8E4Ca049A0e1Db7D26428;//new TransparentProxyFactory();
+    ITransparentProxyFactory proxyFactory = ITransparentProxyFactory(0x98C977c66266366dbEc8E4Ca049A0e1Db7D26428);//new TransparentProxyFactory();
     address proxyAdmin = 0x48a93f60B3F3f741d864Fdaf6E85A5634e71d1D8;//proxyFactory.createProxyAdmin(msg.sender);
 
     address aaveToken = proxyFactory
@@ -50,8 +49,8 @@ contract DeployAaveV3Mock is Script {
     );
 
     vm.stopBroadcast();
-    console.log('aave token', aaveToken);
-    console.log('proxyAdmin', proxyAdmin);
-    console.log('proxyFactory', address(proxyFactory));
+//    console.log('aave token', aaveToken);
+//    console.log('proxyAdmin', proxyAdmin);
+//    console.log('proxyFactory', address(proxyFactory));
   }
 }
